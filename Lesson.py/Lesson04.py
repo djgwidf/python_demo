11a0c73ac38581f267f26cf4a7f787cd8fb8b6b4
djgwidf <pixel4a0343@gmail.com>
2022-03-19 21:53:11 +0900

    update

---
+++ Lesson.py/Lesson04.py
@@ -0,0 +1,23 @@
+x =10
+y =2
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+
+print(x>y)
+print(x<y)
+print(x <= y)
+print(x >= y)
+print(x != y)
+print(x == y)
+
+x = 8
+y = 3
+print(x >= 5 and x <= 10)
+print(y >= 5 and y <= 10)
+
+print(x == 3 or y == 3)
+print(x == 1 or y == 1)
