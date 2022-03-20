3218a30d8fa9f0453b489b5d3e684a9c83a52a2a
djgwidf <pixel4a0343@gmail.com>
2022-03-20 21:31:37 +0900

    update

---
+++ Lesson02/Lesson08.py
@@ -0,0 +1,35 @@
+def say():
+  print("hello world")
+
+say()
+say()
+say()
+
+def say(greeting):
+  print(greeting)
+
+say("hello_world")
+
+def say(greeting):
+  print(greeting)
+
+hello = say
+hello("good morning")
+
+def add(num01, num02):
+  return(num01 + num02)
+
+print(add(6,2))
+
+def add(num01, num02):
+  return(num01 + num02)
+
+add_rsult = add(6,2)
+print(add_rsult)
+
+
+def add (num01, num02, num03):
+  return(num01 + num02 + num03)/3
+
+add_result= add(4,9,2)
+print(add_result)
