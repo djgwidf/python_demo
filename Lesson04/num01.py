1b2fbd95e921cf2239ed279ca89c18750ba72dd7
djgwidf <pixel4a0343@gmail.com>
2022-03-21 21:38:00 +0900

    数あてゲーム

---
+++ Lesson04/num01.py
@@ -0,0 +1,19 @@
+print("数あてゲームを始めます")
+print("答えの範囲は1~100です")
+answer = 10
+
+guess = int(input("あなたが予想する数字："))
+tries = 1
+
+while(guess != answer):
+  if(guess > answer):
+    print("あなたの予想した数は答えより大きいです")
+  else:
+    print("あなたの予想した数は答えより小さいです")
+
+  tries = tries + 1
+  guess = int(input("あなたが予想する数字："))
+
+
+print("正解です。答えは{}".format(answer))
+print("あなたの試行回数は{}でした".format(tries))
