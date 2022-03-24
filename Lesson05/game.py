0ee3f12b4bd42b0cc9db00c0a4db73c3e06c79e4
djgwidf <pixel4a0343@gmail.com>
2022-03-24 21:57:15 +0900

    updata

--- Lesson05/game.py
+++ Lesson05/game.py
@@ -1,12 +1,11 @@
 from tkinter import *
 
- # ボール辞書型データ
 ball = {
-            "dirx": 15, # X方向のボールの速さ
-            "diry": -15,  # Y方向のボールの速さ
-            "x": 350, # ボールの位置
+            "dirx": 15, # X方向速さ
+            "diry": -15,  # Y方向速さ
+            "x": 350, # 位置
             "y": 300,
-            "w": 10, # ボールの幅
+            "w": 10, # 幅
            }
 
  # ウィンドウ
@@ -28,7 +27,7 @@ def move_ball():
      # 仮の変数に移動後の値を記録
      bx = ball["x"] + ball["dirx"]
      by = ball["y"] + ball["diry"]
-     # 上左右の壁に当たった？
+     # 上左右の壁
      if bx < 0 or bx > 600: ball["dirx"] *= -1
      if by < 0 or by > 400: ball["diry"] *= -1
      # 移動内容を反映
@@ -42,4 +41,5 @@ def game_loop():
      win.after(50, game_loop)
 
 game_loop()
-win.mainloop() # ゲームウィンドウ
+win.mainloop()
+
