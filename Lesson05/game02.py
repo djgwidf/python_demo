826098a6d2f4112d3dc408a3fbbfe4a36d18f404
djgwidf <pixel4a0343@gmail.com>
2022-03-24 22:34:06 +0900

    ball move

--- Lesson05/game02.py
+++ Lesson05/game02.py
@@ -8,7 +8,32 @@ win.resizable(False, False)
 can = tk.Canvas(bg="gray", width=400, height=600)
 can.place(x=0, y=0)
 
-#玉
-can.create_oval(50, 500, 70, 520, fill= "white")
+# 玉
+ball_x = 50
+ball_y = 500
+bx = 5
+by = -5
+
+def drawBall():
+    global ball_x
+    global ball_y
+    global bx
+    global by
+    can.create_oval(ball_x, ball_y, ball_x+20, ball_y+20, fill="white")
+    if ball_x <= 0 or ball_x >= 380:
+      bx *= -1
+    if ball_y <= 0 or ball_y >= 580:
+      by *= -1
+    ball_x += bx
+    ball_y -= by
+
+
+def gameLoop():
+    can.delete("all")
+    drawBall()
+    win.after(30, gameLoop)
+
+
+gameLoop()
 
 win.mainloop()
