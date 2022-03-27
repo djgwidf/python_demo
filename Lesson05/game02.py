7740c193f399ee0f783d0cc4a3a8a7284a28ae73
djgwidf <pixel4a0343@gmail.com>
2022-03-27 17:55:46 +0900

    bind

--- Lesson05/game02.py
+++ Lesson05/game02.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-
+from tkinter import messagebox
 win = tk.Tk()
 win.title("BLOCK")
 win.geometry("405x605")
@@ -7,6 +7,12 @@ win.resizable(False, False)
 
 can = tk.Canvas(bg="gray", width=400, height=600)
 can.place(x=0, y=0)
+
+
+def gameOver():
+    messagebox.showinfo("Infomation", "GAME OVER")
+    exit()
+
 
 # 玉
 ball_x = 50
@@ -16,14 +22,15 @@ by = -5
 
 
 def drawBall():
-    global ball_x
-    global ball_y
-    global bx
-    global by
+    global ball_x, ball_y, bx, by
     can.create_oval(ball_x, ball_y, ball_x+20, ball_y+20, fill="white")
     if ball_x <= 0 or ball_x >= 380:
         bx *= -1
     if ball_y <= 0 or ball_y >= 580:
+        by *= -1
+    if ball_y >= 580:
+        gameOver()
+    if ball_y >= 480 and ball_x >= rack_x-10 and ball_x <= rack_x+10:
         by *= -1
     ball_x += bx
     ball_y -= by
@@ -33,18 +40,28 @@ def drawBall():
 rack_x = 170
 keyPress_R = False
 keyPress_L = False
+
+
 def rightKeyPress(event):
-  global keyPress_R
-  keyPress_R = True
+    global keyPress_R
+    keyPress_R = True
+
+
 def rightKeyRelease(event):
-  global keyPress_R
-  keyPress_R = False
+    global keyPress_R
+    keyPress_R = False
+
+
 def leftKeyPress(event):
-  global keyPress_L
-  keyPress_L = True
+    global keyPress_L
+    keyPress_L = True
+
+
 def leftKeyRelease(event):
-  global keyPress_L
-  keyPress_L = False
+    global keyPress_L
+    keyPress_L = False
+
+
 win.bind("<KeyPress-Right>", rightKeyPress)
 win.bind("<KeyRelease-Right>", rightKeyRelease)
 win.bind("<KeyPress-Left>", leftKeyPress)
@@ -55,9 +72,9 @@ def drawRacket():
     global rack_x
     can.create_rectangle(rack_x, 500, rack_x+60, 520, fill="white")
     if keyPress_R == True and rack_x <= 350:
-      rack_x += 5
+        rack_x += 5
     if keyPress_L == True and rack_x >= -10:
-      rack_x -= 5
+        rack_x -= 5
 # 表示
 
 
