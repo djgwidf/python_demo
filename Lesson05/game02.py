a44905c175b0ecf869992991719b1f7fe651ec5e
djgwidf <pixel4a0343@gmail.com>
2022-03-27 17:29:54 +0900

    updata

--- Lesson05/game02.py
+++ Lesson05/game02.py
@@ -14,6 +14,7 @@ ball_y = 500
 bx = 5
 by = -5
 
+
 def drawBall():
     global ball_x
     global ball_y
@@ -21,16 +22,49 @@ def drawBall():
     global by
     can.create_oval(ball_x, ball_y, ball_x+20, ball_y+20, fill="white")
     if ball_x <= 0 or ball_x >= 380:
-      bx *= -1
+        bx *= -1
     if ball_y <= 0 or ball_y >= 580:
-      by *= -1
+        by *= -1
     ball_x += bx
     ball_y -= by
+
+
+# 板
+rack_x = 170
+keyPress_R = False
+keyPress_L = False
+def rightKeyPress(event):
+  global keyPress_R
+  keyPress_R = True
+def rightKeyRelease(event):
+  global keyPress_R
+  keyPress_R = False
+def leftKeyPress(event):
+  global keyPress_L
+  keyPress_L = True
+def leftKeyRelease(event):
+  global keyPress_L
+  keyPress_L = False
+win.bind("<KeyPress-Right>", rightKeyPress)
+win.bind("<KeyRelease-Right>", rightKeyRelease)
+win.bind("<KeyPress-Left>", leftKeyPress)
+win.bind("<KeyRelease-Left>", leftKeyRelease)
+
+
+def drawRacket():
+    global rack_x
+    can.create_rectangle(rack_x, 500, rack_x+60, 520, fill="white")
+    if keyPress_R == True and rack_x <= 350:
+      rack_x += 5
+    if keyPress_L == True and rack_x >= -10:
+      rack_x -= 5
+# 表示
 
 
 def gameLoop():
     can.delete("all")
     drawBall()
+    drawRacket()
     win.after(30, gameLoop)
 
 
